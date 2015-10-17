748020a75d878d7e176088ffd2cea55a88c7cb26
Bo <krist.jin.apply@gmail.com>
2015-10-16 21:21:17 -0400

    add CRUD for student

--- router.py
+++ router.py
@@ -3,24 +3,40 @@
 data API
 """
 from flask import Flask, render_template, request, flash, redirect, url_for, jsonify
+import requests
+from settings import NUMBER_OF_SHARD
 
 app = Flask(__name__)
 
-@app.route('/public/student/<uni>', methods=['POST'])
-def create_student(uni):
-    return "create_student: "+str(uni)
+@app.route('/public/student', methods=['POST'])
+def create_student():
+    original_json = request.get_json()
+    try:
+        sanitized_data = getSanitizedJson(original_json)  # for security reason (optional)
+    except Exception, e:
+        return "invalid request"
+    shard_index = uni_hash(uni)
+    return requests.post('/private/student/shard/%d'%shard_index, json=sanitized_data).status_code
 
 @app.route('/public/student/<uni>', methods=['GET'])
 def retrive_student(uni):
-    return "retrive_student: "+str(uni)
+    shard_index = uni_hash(uni)
+    return requests.get('/private/student/shard/%d/uni/%s'%(shard_index,uni)).content
 
 @app.route('/public/student/<uni>', methods=['PUT'])
 def update_student(uni):
-    return "update_student: "+str(uni)
+    original_json = request.get_json()
+    try:
+        sanitized_data = getSanitizedJson(original_json)  # for security reason (optional)
+    except Exception, e:
+        return "invalid request"
+    shard_index = uni_hash(uni)
+    return requests.put('/private/student/shard/%d/uni/%s'%(shard_index,uni), json=sanitized_data).status_code
 
 @app.route('/public/student/<uni>', methods=['DELETE'])
 def delete_student(uni):
-    return "delete_student: "+str(uni)
+    shard_index = uni_hash(uni)
+    return requests.delete('/private/student/shard/%d/uni/%s'%(shard_index,uni)).status_code
 
 
 @app.route('/public/course/<cid>', methods=['POST'])
@@ -55,6 +71,15 @@ def update_registration(rid):
 @app.route('/public/registration/<rid>', methods=['DELETE'])
 def delete_registration(rid):
     return "delete_registration: "+str(rid)
+
+def uni_hash(uni):
+    return uni.__hash__()%NUMBER_OF_SHARD
+
+def getSanitizedJson(original_json):
+    """
+    used for security check
+    """
+    return original_json
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
