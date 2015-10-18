8311ff12d49dbba0cc417447744bbb1df52836c7
shqyking <sunqyoung@gmail.com>
2015-10-18 15:20:50 -0400

    Add sending DELETE request to registration service when deleting student information

--- student_flask.py
+++ student_flask.py
@@ -1,10 +1,12 @@
 from flask import Flask, request
+from pymongo import MongoClient
 import urllib2, json, requests
 import sys, subprocess
 import os
 
 app = Flask(__name__)
 eve_url = ''
+mongo_url = 'mongodb://admin:admin@ds039684.mongolab.com:39684/project1'
 
 #Just redirect GET request to eve service
 @app.route("/private/student/<uni>", methods=['GET'])
@@ -18,18 +20,28 @@ def add_student():
 	response = requests.post(eve_url, data=request.get_json())
 	return response.content
 
-#Get student id and etag first. Then delete it.
+#Get student id and etag first. Then delete student and all registration information
 @app.route("/private/student/<uni>", methods=['DELETE'])
 def delete_student(uni):
 	#Get student information
 	response = requests.get(eve_url + uni)
 	student_info = response.json()
-	if response.status_code != 200:
+	if response.status_code > 206:    #Fail to get student information by uni
 		return response.content
+	#Get registration status from MongoDB instance_info collection. If registration service is not running, return
+	client = MongoClient(mongo_url)
+	regis_info = client.project1.instance_info.find({'instanceType' : 'registration'})
+	if(regis_info.count() == 0):
+		return 'Error : Registartion service is not running'
+	registration_url = 'http://' + regis_info[0]['host'] + ':' + regis_info[0]['port'] + '/private/registration/uni/'
+	client.close()
 	#Send DELETE request to eve service to delete student
 	response = requests.delete(eve_url + student_info['_id'], headers={"If-Match" : student_info['_etag']})
+	if response.status_code > 206:    #Failed to delete student information
+		return response.content
 	#Send DELETE request to registration service
-	return response.content
+	response2 = requests.delete(registration_url + uni)
+	return response.content + '\n\n' + response2.content
 
 #Get student id and etag first. Then update it. 
 @app.route("/private/student/<uni>", methods=['PATCH'])
@@ -38,7 +50,7 @@ def update_student(uni):
 	response = requests.get(eve_url + uni)
 	student_info = response.json()
 	print request.get_json()
-	if response.status_code != 200:
+	if response.status_code > 206:    #Fail to get student information by uni
 		return response.content
 	#Send PATCH request to eve service to update student information
 	response = requests.patch(eve_url + student_info['_id'], data=request.get_json(), headers={"If-Match" : student_info['_etag']})
