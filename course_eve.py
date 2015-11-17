3145758fa502b25cf29b7e0e79feb51528fd8de2
jiu9x9uij <jiu9x9uij@gmail.com>
2015-11-16 22:38:21 -0500

    New Course Service
    * Split into eve (resource management) and flask (route forward + schema
    management)
    * Reads registration service info from db
    * Reads schema info from db
    * Supports schema CRUD
    * Restarts itself when schema is modified
    ISSUES:
    * Reg expression for call_number rules out invalid patterns with less
    than 5 digits but does not rule out patterns with more than 5 digits.
    Should match only exactly 5 digits.
    * Schema loading in flask seems to be executed for twice, probably
    because of subprocessing.

---
+++ course_eve.py
@@ -0,0 +1,165 @@
+from eve import Eve
+from flask import Response, request
+from pymongo import MongoClient
+import requests, json, sys
+
+mongo_url = 'mongodb://admin:admin@ds039684.mongolab.com:39684/project1'
+
+
+
+#####################
+# Eve Configuration #
+#####################
+# Get course schema from instance_info db
+try:
+	client = MongoClient(mongo_url)
+	db = client.project1
+	cursor = db.schema.find({"name": "course"})
+	client.close()
+	if cursor.count() < 1:
+		raise ConfigurationUnavailable
+	# print cursor[0]
+	#print '========= SCHEMA =========\n%s\n==========================' % (cursor[0]['value'])
+	schema = cursor[0]['value']
+except Exception as e:
+	if type(e).__name__ == 'ConnectionError':
+		print 'Error: Cannot connect to mongodb'
+		raise MongoDbUnavailable
+	else:
+		print 'Error: %s when getting schema from mongodb' % (type(e).__name__)
+		raise e
+
+# Settings
+settings = {
+	# Use db hosted on MongoLab
+	'MONGO_HOST': 'ds039684.mongolab.com',
+	'MONGO_PORT': 39684,
+	'MONGO_USERNAME': 'admin',
+	'MONGO_PASSWORD': 'admin',
+	'MONGO_DBNAME': 'project1',
+	# URL prefix
+	'URL_PREFIX': 'private',
+
+	# Supports ../course/<call_number> where <call_number> consists of 5 digits
+	'ID_FIELD': 'call_number',
+	'ITEM_LOOKUP_FIELD': 'call_number',
+	'ITEM_URL': 'regex("[0-9]{5}")',
+	
+	# Data schema
+	'DOMAIN': {
+		'course': {
+			'additional_lookup': {
+				 'url': 'regex("[0-9]{5}")', #("[\w]+")',
+				 'field': 'call_number',
+			 },
+			 
+			'schema': schema
+		}
+	},
+	
+	'RESOURCE_METHODS': ['GET', 'POST'],
+	'ITEM_METHODS': ['GET', 'PUT', 'DELETE']
+}
+
+# App Initialization
+app = Eve(settings=settings)
+
+
+
+########################
+# Custom Error Handler #
+########################
+class MongoDbUnavailable(Exception):
+	pass
+
+class ConfigurationUnavailable(Exception):
+	pass
+
+class RegistrationServiceUnavailable(Exception):
+	pass
+
+class RegistrationServiceError(Exception):
+	pass
+
+@app.errorhandler(MongoDbUnavailable)
+def mongodb_failed_to_connect(error):
+	return Response('{"_status": "ERR", "_error": {"message": "Failed to connect to mongodb", "code": 500}}', mimetype='application/json', status=500)
+
+@app.errorhandler(ConfigurationUnavailable)
+def mongodb_configuration_unavailable(error):
+	return Response('{"_status": "ERR", "_error": {"message": "Failed to read configuration of course service from mongodb", "code": 500}}', mimetype='application/json', status=500)
+
+@app.errorhandler(RegistrationServiceUnavailable)
+def registration_failed_to_connect(error):
+	return Response('{"_status": "ERR", "_error": {"message": "Failed to connect to registration service", "code": 504}}', mimetype='application/json', status=504)
+
+@app.errorhandler(RegistrationServiceError)
+def registration_failed_to_delete(error):
+	return Response('{"_status": "ERR", "_error": {"message": "Failed to delete related registration information", "code": 502}}', mimetype='application/json', status=502)
+
+@app.errorhandler(Exception)
+def unexpected_failure(error):
+    template = "An exception of type {0} occured. Arguments:\n{1!r}"
+    message = template.format(type(error).__name__, error.args)
+    print message
+    return Response('{"_status": "ERR", "_error": {"message": "Unexpected failure", "code": 500}}', mimetype='application/json', status=500)
+
+
+
+##################
+# Event handlers #
+##################
+def pre_DELETE_callback(resource, request, lookup):
+	print 'Received DELETE request, resource = %s, lookup = %s' % (resource, lookup)
+	if resource != 'course':
+		return
+
+	# Get registration host and port from instance_info db
+	try:
+		client = MongoClient(mongo_url)
+		db = client.project1
+		cursor = db.instance_info.find({"instanceType": "registration"})
+		client.close()
+		if cursor.count() < 1:
+			raise RegistrationServiceUnavailable
+		else:
+			# print cursor[0]
+			registration_service_url = 'http://' + cursor[0]['host'] + ':' + str(cursor[0]['port']) + '/private/registration/uni/'
+		print 'requesting %s"%s"' % (registration_service_url, lookup['call_number'])
+	except Exception as e:
+		if type(e).__name__ == 'ConnectionError':
+			print 'Error: Cannot connect to mongodb'
+			raise MongoDbUnavailable
+		else:
+			print 'Error: %s when getting registration_service_url from mongodb' % (type(e).__name__)
+			raise e
+
+	# Try delete from registration before delete locally
+	try:
+		response = requests.delete(registration_service_url + '"' + lookup['call_number'] + '"')
+		# print response.__dict__['status_code']
+		# print response.json # Response body
+		status_code = response.__dict__['status_code']
+		if status_code != 200 and status_code != 404:
+			print "Exception: Failed to delete related registration information"
+			raise RegistrationServiceError
+	except Exception as e:
+		if type(e).__name__ == 'ConnectionError':
+			print 'Error: Registartion service is not running'
+			raise RegistrationServiceUnavailable
+		else:
+			print 'Error: %s when deleting %s from registration' % (type(e).__name__, lookup['call_number'])
+			raise e
+
+
+
+
+
+# Main
+if __name__ == '__main__':
+	app.on_pre_DELETE += pre_DELETE_callback
+	
+	if len(sys.argv) == 3:
+		app.run(host = sys.argv[1], port = int(sys.argv[2]))
+	else:
+		app.run()
---
+++ course_flask.py
@@ -0,0 +1,340 @@
+from flask import Flask, request, Response
+from pymongo import MongoClient
+import course_flask as cf
+import urllib2, json, requests
+import sys, subprocess, signal, time
+import os
+import re
+
+mongo_url = 'mongodb://admin:admin@ds039684.mongolab.com:39684/project1'
+
+
+
+#############
+# Variables #
+#############
+global eve_process
+global args
+app = Flask(__name__)
+eve_base_url = ''
+call_number_pattern = re.compile("[0-9]{5}")
+# Get course schema from instance_info db
+try:
+	client = MongoClient(mongo_url)
+	db = client.project1
+	cursor = db.schema.find({"name": "course"})
+	client.close()
+	if cursor.count() < 1:
+		raise ConfigurationUnavailable
+	# print cursor[0]
+	print '========= SCHEMA =========\n%s\n==========================' % (cursor[0]['value'])
+	schema = cursor[0]['value']
+except Exception as e:
+	if type(e).__name__ == 'ConnectionError':
+		print 'Error: Cannot connect to mongodb'
+		raise MongoDbUnavailable
+	else:
+		print 'Error: %s when getting schema from mongodb' % (type(e).__name__)
+		raise e
+
+########################
+# Custom Error Handler #
+########################
+class MongoDbUnavailable(Exception):
+	pass
+
+class ConfigurationUnavailable(Exception):
+	pass
+
+class EveUnavailable(Exception):
+	pass
+
+@app.errorhandler(MongoDbUnavailable)
+def mongodb_failed_to_connect(error):
+	return Response('{"_status": "ERR", "_error": {"message": "Failed to connect to mongodb", "code": 500}}', mimetype='application/json', status=500)
+
+@app.errorhandler(ConfigurationUnavailable)
+def mongodb_configuration_unavailable(error):
+	return Response('{"_status": "ERR", "_error": {"message": "Failed to read configuration of course service from mongodb", "code": 500}}', mimetype='application/json', status=500)
+
+@app.errorhandler(EveUnavailable)
+def mongodb_failed_to_connect(error):
+	return Response('{"_status": "ERR", "_error": {"message": "Failed to connect to eve service", "code": 500}}', mimetype='application/json', status=500)
+
+@app.errorhandler(Exception)
+def unexpected_failure(error):
+    template = "An exception of type {0} occured. Arguments:\n{1!r}"
+    message = template.format(type(error).__name__, error.args)
+    print message
+    return Response('{"_status": "ERR", "_error": {"message": "Unexpected failure", "code": 500}}', mimetype='application/json', status=500)
+
+
+
+###############
+# API Mapping #
+###############
+@app.route("/private/course", methods = ['GET'])
+@app.route("/private/course/", methods = ['GET'])
+def course_read_all():
+	print 'Recieved GET course request'
+	try:
+		response = requests.get(eve_base_url)
+		return Response(response.content, mimetype='application/json', status=response.status_code)
+	except Exception as e:
+		if type(e).__name__ == 'ConnectionError':
+			print 'Error: Cannot connect to eve'
+			raise EveUnavailable
+		else:
+			print 'Error: %s when add course to eve' % (type(e).__name__)
+			raise e
+
+@app.route("/private/course/<call_number>", methods = ['GET'])
+@app.route("/private/course/<call_number>/", methods = ['GET'])
+def course_read_specific(call_number):
+	print 'Recieved GET specific course request'
+	try:
+		response = requests.get(eve_base_url + '/' + str(call_number))
+		return Response(response.content, mimetype='application/json', status=response.status_code)
+	except Exception as e:
+		if type(e).__name__ == 'ConnectionError':
+			print 'Error: Cannot connect to eve'
+			raise EveUnavailable
+		else:
+			print 'Error: %s when add course to eve' % (type(e).__name__)
+			raise e
+
+@app.route("/private/course", methods = ['POST'])
+@app.route("/private/course/", methods = ['POST'])
+def course_create():
+	print 'Recieved POST course request'
+	try:
+		content = request.get_json(force = True)
+		print 'content(type=%s) = %s' % (type(content).__name__, content)
+		count_invalid = 0
+		if (type(content).__name__ == 'list'):
+			# Multiple courses, validate each course's call_number format
+			for course in content:
+				print 'course = %s' % (course)
+				for key in course:
+					if key == 'call_number' and not call_number_pattern.match(course[key]):
+						print '\tinvalid call_number'
+						content.remove(course)
+						count_invalid += 1
+		else:
+			# Single course, validate call_number format
+			print type(content).__name__
+			for key in content:
+				# Loop in case of invalid input; invalid input will be handled by eve, here only validate format
+				print 'key = %s' % (key)
+				if key == 'call_number' and not call_number_pattern.match(content[key]):
+					print '\tinvalid call_number'
+					content = []
+					count_invalid += 1
+					break
+		print 'A content(type=%s) = %s' % (type(content).__name__, content)
+		if len(content) == 0:
+			# No valid data left, return error
+			return Response('{"_status": "ERR", "_issues": {"call_number": "value format must be of five digits"},"_error": {"message": "Insertion failure: ' + str(count_invalid) + ' document(s) contain(s) error(s)", "code": 500}}', mimetype='application/json', status=422)
+		else:
+			# Otherwise, ignore invalid data and process valid data
+			headers = {'content-type': 'application/json'}
+			response = requests.post(eve_base_url, data=json.dumps(content), headers = headers)
+			return Response(response.content, mimetype='application/json', status=response.status_code)
+	except Exception as e:
+		if type(e).__name__ == 'ConnectionError':
+			print 'Error: Cannot connect to eve'
+			raise EveUnavailable
+		else:
+			print 'Error: %s when add course to eve' % (type(e).__name__)
+			raise e
+
+@app.route("/private/course/<call_number>", methods = ['DELETE'])
+@app.route("/private/course/<call_number>/", methods = ['DELETE'])
+def delete_registration_for_uni(call_number):
+	print 'Recieved DELETE specific schema request'
+	try:
+		response = requests.delete(eve_base_url + '/' + str(call_number), headers = request.headers)
+		return Response(response.content, mimetype='application/json', status=response.status_code)
+	except Exception as e:
+		if type(e).__name__ == 'ConnectionError':
+			print 'Error: Cannot connect to eve'
+			raise EveUnavailable
+		else:
+			print 'Error: %s when add course to eve' % (type(e).__name__)
+			raise e
+
+
+
+##################
+# Schema Related #
+##################
+@app.route("/private/course/schema", methods = ['GET'])
+def schema_view():
+		print 'Recieved GET schema request'
+		return Response(json.dumps(schema, ensure_ascii=False), mimetype='application/json', status=200)
+
+@app.route("/private/course/schema", methods = ['POST'])
+def schema_add_column():
+		print 'Recieved POST schema request'
+		try:
+			columns_to_add = request.get_json(force = True)
+			print 'columns_to_add(type=%s) = %s' % (type(columns_to_add).__name__, columns_to_add)
+			# Process only new attributes according to local copy of schema
+			client = MongoClient(mongo_url)
+			db = client.project1
+			count = 0
+			for column in columns_to_add:
+				print "column = %s" % (column)
+				if column not in schema:
+					print "\tadd"
+					# Update db copy
+					result = db.schema.update_one(
+						{"name": "course"},
+					    {
+					        "$set": {
+					            "value."+column: columns_to_add[column]
+					        },
+					        "$currentDate": {"lastModified": True}
+					    }
+					)
+					# Update local copy
+					schema.update({column: columns_to_add[column]})
+					count += 1			
+			client.close()
+			if count > 0:
+				print 'result.matched_count = %s' % (result.matched_count)
+				if result.matched_count < 1:
+					raise ConfigurationUnavailable
+				else:
+					# Schema modified, restart eve
+					stop_eve_process()
+					time.sleep(0.1)
+					start_eve_process()
+			return Response('{"_status": "SUCCESS", "_success": {"message": "'+str(count)+' column(s) added", "code": 200}}', mimetype='application/json', status=200)
+		except Exception as e:
+			if type(e).__name__ == 'ConnectionError':
+				print 'Error: Cannot connect to mongodb'
+				raise MongoDbUnavailable
+			else:
+				print 'Error: %s when adding column to schema' % (type(e).__name__)
+				raise e
+
+@app.route("/private/course/schema", methods = ['PUT'])
+def schema_update_column():
+		print 'Recieved PUT schema request'
+		try:
+			columns_to_update = request.get_json(force = True)
+			print 'columns_to_update(type=%s) = %s' % (type(columns_to_update).__name__, columns_to_update)
+			# Process only existing attributes according to local copy of schema
+			client = MongoClient(mongo_url)
+			db = client.project1
+			count = 0
+			for column in columns_to_update:
+				print "column = %s" % (column)
+				if column in schema:
+					print "\tupdate"
+					# Forbid modification of lookup field
+					if column == 'call_number':
+						return Response('{"_status": "ERR", "_error": {"message": "Modification of lookup field is not allowed", "code": 405}}', mimetype='application/json', status=405)
+					# Update db copy
+					result = db.schema.update_one(
+						{"name": "course"},
+					    {
+					        "$set": {
+					            "value."+column: columns_to_update[column]
+					        },
+					        "$currentDate": {"lastModified": True}
+					    }
+					)
+					# Update local copy
+					schema.update({column: columns_to_update[column]})
+					count += 1
+			client.close()
+			if count > 0:
+				print 'result.matched_count = %s' % (result.matched_count)
+				if result.matched_count < 1:
+					raise ConfigurationUnavailable
+				else:
+					# Schema modified, restart eve
+					stop_eve_process()
+					time.sleep(0.1)
+					start_eve_process()
+			return Response('{"_status": "SUCCESS", "_success": {"message": "'+str(count)+' column(s) updated", "code": 200}}', mimetype='application/json', status=200)
+		except Exception as e:
+			if type(e).__name__ == 'ConnectionError':
+				print 'Error: Cannot connect to mongodb'
+				raise MongoDbUnavailable
+			else:
+				print 'Error: %s when updating column of schema' % (type(e).__name__)
+				raise e
+
+@app.route("/private/course/schema", methods = ['DELETE'])
+def schema_delete_column():
+		print 'Recieved DELETE schema request'
+		try:
+			columns_to_remove = request.get_json(force = True)
+			print 'columns_to_remove(type=%s) = %s' % (type(columns_to_remove).__name__, columns_to_remove)
+			# Process only existing attributes according to local copy of schema
+			client = MongoClient(mongo_url)
+			db = client.project1
+			count = 0
+			for column in columns_to_remove:
+				print "column = %s" % (column)
+				if column in schema:
+					print "\tremove"
+					# Forbid removal of lookup field
+					if column == 'call_number':
+						return Response('{"_status": "ERR", "_error": {"message": "Removal of lookup field is not allowed", "code": 405}}', mimetype='application/json', status=405)
+					# Update db copy
+					result = db.schema.update_one(
+						{"name": "course"},
+					    {
+					        "$unset": {
+					            "value."+column: ""
+					        },
+					        "$currentDate": {"lastModified": True}
+					    }
+					)
+					# Update local copy
+					del schema[column]
+					count += 1
+			client.close()
+			if count > 0:
+				print 'result.matched_count = %s' % (result.matched_count)
+				if result.matched_count < 1:
+					raise ConfigurationUnavailable
+				else:
+					# Schema modified, restart eve
+					stop_eve_process()
+					time.sleep(0.1)
+					start_eve_process()
+			return Response('{"_status": "SUCCESS", "_success": {"message": "'+str(count)+' column(s) deleted", "code": 200}}', mimetype='application/json', status=200)
+		except Exception as e:
+			if type(e).__name__ == 'ConnectionError':
+				print 'Error: Cannot connect to mongodb'
+				raise MongoDbUnavailable
+			else:
+				print 'Error: %s when deleting column from schema' % (type(e).__name__)
+				raise e
+
+
+
+def start_eve_process():
+	print "starting course eve process..."
+	cf.eve_process = subprocess.Popen(args)
+
+def stop_eve_process():
+	print "stopping course eve process..."
+	os.kill(cf.eve_process.pid, signal.SIGTERM)
+
+if __name__ == "__main__":
+		if(len(sys.argv) >= 3):
+			host = sys.argv[1]
+			#I set eve service runs on different port. If current flask runs on 5000 port, eve runs on 15000 port
+			eve_port = str((int(sys.argv[2]) + 10000))
+			eve_base_url = 'http://' + host + ':' + eve_port + '/private/course'
+			args = ['python', 'course_eve.py', host, eve_port]
+			#run eve service as subprocess in background
+			start_eve_process()
+			app.run(host=host, port=int(sys.argv[2]))
+
